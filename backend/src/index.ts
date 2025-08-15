79ba7d9af9b652a3b337d2bb6ba1db737239f289
samratkr <sonukrsharma619@gmail.com>
2025-08-15 08:47:24 +0530

    cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
